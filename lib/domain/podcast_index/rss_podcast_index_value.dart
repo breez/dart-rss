0586eb3d169d5e84bc467261214262e628a8057e
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-11-17 11:39:47 -0300

    Add value time split

--- lib/domain/podcast_index/rss_podcast_index_value.dart
+++ lib/domain/podcast_index/rss_podcast_index_value.dart
@@ -4,13 +4,15 @@ class RssPodcastIndexValue {
   final String? type;
   final String? method;
   final String? suggested;
-  final List<RssPodcastIndexValueRecipient?> recipients;
+  final List<RssPodcastIndexValueRecipient> recipients;
+  final List<RssPodcastIndexValueTimeSplit> timeSplits;
 
-  RssPodcastIndexValue({
+  const RssPodcastIndexValue({
     this.type,
     this.method,
     this.suggested,
     this.recipients = const <RssPodcastIndexValueRecipient>[],
+    this.timeSplits = const <RssPodcastIndexValueTimeSplit>[],
   });
 
   static RssPodcastIndexValue? parse(XmlElement? element) {
@@ -20,10 +22,8 @@ class RssPodcastIndexValue {
       type: element.getAttribute('type'),
       method: element.getAttribute('method'),
       suggested: element.getAttribute('suggested'),
-      recipients: element
-          .findElements('podcast:valueRecipient')
-          .map((element) => RssPodcastIndexValueRecipient.parse(element))
-          .toList(),
+      recipients: RssPodcastIndexValueRecipient.parseList(element) ?? [],
+      timeSplits: RssPodcastIndexValueTimeSplit.parseList(element) ?? [],
     );
   }
 }
@@ -37,7 +37,7 @@ class RssPodcastIndexValueRecipient {
   final String? split;
   final String? fee;
 
-  RssPodcastIndexValueRecipient({
+  const RssPodcastIndexValueRecipient({
     this.name,
     this.type,
     this.address,
@@ -60,5 +60,86 @@ class RssPodcastIndexValueRecipient {
       fee: element.getAttribute('fee'),
     );
   }
+
+  static List<RssPodcastIndexValueRecipient>? parseList(XmlElement? element) {
+    if (element == null) return null;
+    return element
+        .findElements('podcast:valueRecipient')
+        .map((element) => RssPodcastIndexValueRecipient.parse(element))
+        .whereType<RssPodcastIndexValueRecipient>()
+        .toList();
+  }
 }
 
+class RssPodcastIndexValueTimeSplit {
+  final double startTime;
+  final double duration;
+  final int remoteStartTime;
+  final int remotePercentage;
+  final List<RssPodcastIndexValueRecipient> valueRecipients;
+  final List<RssPodcastIndexRemoteItem> remoteItems;
+
+  const RssPodcastIndexValueTimeSplit({
+    required this.startTime,
+    required this.duration,
+    this.remoteStartTime = 0,
+    this.remotePercentage = 100,
+    this.valueRecipients = const <RssPodcastIndexValueRecipient>[],
+    this.remoteItems = const <RssPodcastIndexRemoteItem>[],
+  });
+
+  static RssPodcastIndexValueTimeSplit? parse(XmlElement? element) {
+    if (element == null) return null;
+
+    return RssPodcastIndexValueTimeSplit(
+      startTime: double.tryParse(element.getAttribute('startTime') ?? '') ?? 0,
+      duration: double.tryParse(element.getAttribute('duration') ?? '') ?? 0,
+      remoteStartTime:
+          int.tryParse(element.getAttribute('remoteStartTime') ?? '') ?? 0,
+      remotePercentage:
+          int.tryParse(element.getAttribute('remotePercentage') ?? '') ?? 100,
+      valueRecipients: RssPodcastIndexValueRecipient.parseList(element) ?? [],
+      remoteItems: RssPodcastIndexRemoteItem.parseList(element) ?? [],
+    );
+  }
+
+  static List<RssPodcastIndexValueTimeSplit>? parseList(XmlElement? element) {
+    if (element == null) return null;
+    return element
+        .findElements('podcast:valueTimeSplit')
+        .map((element) => RssPodcastIndexValueTimeSplit.parse(element))
+        .whereType<RssPodcastIndexValueTimeSplit>()
+        .toList();
+  }
+}
+
+class RssPodcastIndexRemoteItem {
+  final String itemGuid;
+  final String feedGuid;
+  final String medium;
+
+  const RssPodcastIndexRemoteItem({
+    required this.itemGuid,
+    required this.feedGuid,
+    required this.medium,
+  });
+
+  static RssPodcastIndexRemoteItem? parse(XmlElement? element) {
+    if (element == null) return null;
+
+    return RssPodcastIndexRemoteItem(
+      itemGuid: element.getAttribute('itemGuid') ?? '',
+      feedGuid: element.getAttribute('feedGuid') ?? '',
+      medium: element.getAttribute('medium') ?? '',
+    );
+  }
+
+  static List<RssPodcastIndexRemoteItem>? parseList(XmlElement? element) {
+    if (element == null) return null;
+    return element
+        .findElements('podcast:remoteItem')
+        .map((element) => RssPodcastIndexRemoteItem.parse(element))
+        .whereType<RssPodcastIndexRemoteItem>()
+        .toList();
+  }
+}
